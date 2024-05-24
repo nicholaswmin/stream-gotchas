5979c3b8e0c546f09b37be705dd40ffaae152709
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-05-24 15:16:31 +0300

    tweak dbinit script

--- initdb.js
+++ initdb.js
@@ -2,28 +2,39 @@ import knex from 'knex'
 
 const { DATABASE_URL } = process.env
 const name = DATABASE_URL.split('/')[3]
-
-const isLocal = DATABASE_URL.includes('@localhost')
+const isLocalDB = DATABASE_URL.includes('@localhost')
 const adminURL = DATABASE_URL.replace(name, '')
 const userURL = DATABASE_URL
+const sslOpts = isLocalDB ? false : { rejectUnauthorized: false }
 
-const [ admin, db ] = [
-  knex({ client: 'pg', connection: { connectionString: adminURL } }),
-  knex({ client: 'pg', connection: { connectionString: userURL } })
-]
+if (isLocalDB) {
+  const admin = knex({
+    client: 'pg',
+    connection: {
+      connectionString: adminURL,
+      ssl: isLocalDB ? false : { rejectUnauthorized: false }
+    }
+  })
 
-if (isLocal) {
   await admin.raw('DROP DATABASE IF EXISTS ?? WITH (FORCE);', [ name ])
   await admin.raw('CREATE DATABASE ??;', [ name ])
+  await admin.destroy()
 }
 
-await db.schema.dropTableIfExists('messages')
-await db.schema.createTable('messages', t => {
+const user = knex({
+  client: 'pg',
+  connection: {
+    connectionString: userURL,
+    ssl: isLocalDB ? false : { rejectUnauthorized: false }
+  }
+})
+
+await user.schema.dropTableIfExists('messages')
+await user.schema.createTable('messages', t => {
   t.increments('id_message').primary()
   t.text('text')
 })
-
-await db.batchInsert('messages', Array(100).fill({ text: 'hello world' }))
-await Promise.all([ admin.destroy(), db.destroy() ])
+await user.batchInsert('messages', Array(100).fill({ text: 'hello world' }))
+await user.destroy()
 
 console.log('Done! DB ready')
